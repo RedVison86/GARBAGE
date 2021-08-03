83f84ad356344e3a79d633380cc1e86b56a32078
RedVison86 <morssuper30@gmail.com>
2021-08-03 12:23:41 +0700

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -7,19 +7,16 @@ const int W_MAP = 15;
 sf::String Map[H_MAP] =
 {
     "SSSSSSSSSSSSSSS",
-    "S            HS",
-    "SG            S",
-    "S         G   S",
+    "S    G   G   HS",
+    "SG           GS",
+    "S      G  G   S",
     "S             S",
-    "S         G   S",
+    "SG   G    G   S",
     "S             S",
-    "S G           S",
-    "S      TT    HS",
+    "S G    G      S",
+    "SG     TT G  HS",
     "SSSSSSSSSSSSSSS",
 };
-
-///////////////////////////////////////////
-
 
 class Model
 {
@@ -29,19 +26,19 @@ class Model
     sf::Sprite spr;
     std::string file;
 
-
     sf::Image mapImg;
     sf::Texture mapTxt;
     sf::Sprite mapSpr;
 
     float x, y, w, h, dx, dy, speed;
-    int dir;
+    int dir, inventory;
 
     Model(std::string F, float X, float Y, float W, float H)
     {
         file = F;
         w = W; h = H; x = X; y = Y;
         dir = 0; speed = 0;
+        inventory = 0;
 
         img.loadFromFile("images/" + file);
         txt.loadFromImage(img);
@@ -60,7 +57,6 @@ class Model
             case 1: dx = -speed; dy = 0; break;
             case 2: dx = 0; dy = speed; break;
             case 3: dx = 0; dy = -speed; break;
-
         }
 
         x += dx * time;
@@ -76,34 +72,22 @@ class Model
         for (int i = y / 32; i < (y + h) / 32; i++)
         for (int j = x / 32; j < (x + w) / 32; j++)
         {
-
             if ((Map[i][j] == 'S') || (Map[i][j] == 'H'))
             {
-                if (dy > 0)
-                {
-                    y = i * 32 - h;
-                }
-
-                if (dy < 0)
-                {
-                    y = i * 32 + 32;
-                }
-
-                if (dx > 0)
-                {
-                    x = j * 32 - w;
-                }
-
-                if (dx < 0)
-                {
-                    x = j * 32 + 32;
-                }
-
+                if (dy > 0)         y = i * 32 - h;
+                else if(dy < 0)     y = i * 32 + 32;
+                else if (dx > 0)    x = j * 32 - w;
+                else if (dx < 0)    x = j * 32 + 32;
             }
 
-            if (Map[i][j] == 'G')
+            if ((Map[i][j] == 'G') && (inventory == 0))
             {
                 Map[i][j] = ' ';
+                inventory++;
+            }
+            else if ((Map[i][j] == 'T') && (inventory > 0))
+            {
+                inventory = 0;
             }
         }
     }
@@ -122,10 +106,7 @@ class View
         Init();
     };
 
-    sf::RenderWindow &window()
-    {
-        return m_window;
-    }
+    sf::RenderWindow &window() { return m_window; }
 
     bool Init()
     {
@@ -146,51 +127,53 @@ class View
         for (int i = 0; i < H_MAP; i++)
         for (int j = 0; j < W_MAP; j++)
         {
-            if (Map[i][j] == 'S')
-                m_model->mapSpr.setTextureRect(sf::IntRect(96,0,32,32));
-
-            if (Map[i][j] == ' ')
-                m_model->mapSpr.setTextureRect(sf::IntRect(0,0,32,32));
-
-            if (Map[i][j] == 'H')
-                m_model->mapSpr.setTextureRect(sf::IntRect(32,0,32,32));
-
-            if (Map[i][j] == 'G')
-                m_model->mapSpr.setTextureRect(sf::IntRect(64,0,32,32));
-
-            if (Map[i][j] == 'T')
-                m_model->mapSpr.setTextureRect(sf::IntRect(128,0,32,32));
-
+            switch(Map[i][j])
+            {
+                case 'S':
+                    m_model->mapSpr.setTextureRect(sf::IntRect(96,0,32,32));
+                    break;
+                case 'H':
+                    m_model->mapSpr.setTextureRect(sf::IntRect(32,0,32,32));
+                    break;
+                case 'G':
+                    m_model->mapSpr.setTextureRect(sf::IntRect(64,0,32,32));
+                    break;
+                case 'T':
+                    m_model->mapSpr.setTextureRect(sf::IntRect(128,0,32,32));
+                    break;
+                case ' ':
+                    m_model->mapSpr.setTextureRect(sf::IntRect(0,0,32,32));
+                    break;
+                default:
+                    break;
+            }
 
             m_model->mapSpr.setPosition(j * 32, i *32);
             window().draw(m_model->mapSpr);
         }
 
         /* draw car */
-        if (m_model->dir == 1)
+        switch(m_model->dir)
         {
-            m_model->spr.setTextureRect(sf::IntRect(68, 17, 34, 17));
-        }
-
-        if (m_model->dir == 0)
-        {
-            m_model->spr.setTextureRect(sf::IntRect(68, 0, 34, 17));
-        }
-
-        if (m_model->dir == 3)
-        {
-            m_model->spr.setTextureRect(sf::IntRect(34, 0, 17, 34));
-        }
-
-        if (m_model->dir == 2)
-        {
-            m_model->spr.setTextureRect(sf::IntRect(51, 0, 17, 34));
+            case 1:
+                m_model->spr.setTextureRect(sf::IntRect(68, 17, 34, 17));
+                break;
+            case 0:
+                m_model->spr.setTextureRect(sf::IntRect(68, 0, 34, 17));
+                break;
+            case 3:
+                m_model->spr.setTextureRect(sf::IntRect(34, 0, 17, 34));
+                break;
+            case 2:
+                m_model->spr.setTextureRect(sf::IntRect(51, 0, 17, 34));
+                break;
+            default:
+                break;
         }
 
         window().draw(m_model->spr);
 
     }
-
 
 };
 
@@ -201,7 +184,7 @@ class Controller
     View *v_view;
 
     public:
-    sf::Event C_event;
+    sf::Event c_event;
     float time;
     sf::Clock clock;
 
@@ -228,15 +211,15 @@ class Controller
 
         if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Up)))
         {
-	    m_model->dir = 3;
+			m_model->dir = 3;
             m_model->speed = 0.1;
-	}
+		}
 
-	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Down)))
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Down)))
         {
-	    m_model->dir = 2;
+			m_model->dir = 2;
             m_model->speed = 0.1;
-	}
+		}
 
         m_model->Logic(time);
     }
@@ -249,9 +232,9 @@ class Controller
             clock.restart();
             time = time / 800;
 
-            while(v_view->window().pollEvent(C_event))
+            while(v_view->window().pollEvent(c_event))
             {
-                if (C_event.type == sf::Event::Closed)
+                if (c_event.type == sf::Event::Closed)
                     v_view->window().close();
             }
 
